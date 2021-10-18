d898264fedfba53f4e0525932d4f30957489d639
103nanami <alice_10373@yahoo.co.jp>
2021-10-18 11:56:21 +0000

    Update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -12,11 +13,12 @@ class BooksController < ApplicationController
   end
 
   def create
+    @books = Book.all
     @book = Book.new(book_params)
     if @book.save
       redirect_to book_path(@book.id)
     else
-      render :new
+      render :index
     end
   end
 
@@ -32,7 +34,7 @@ class BooksController < ApplicationController
  
   private
   def book_params
-    params.permit(:title, :body)
+    params.require(:book).permit(:title, :body)
   end
 
 end

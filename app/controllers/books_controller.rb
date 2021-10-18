375f323e9639fbd79d18beb9040bc78e1b1e3a90
103nanami <alice_10373@yahoo.co.jp>
2021-10-18 10:31:22 +0000

    Update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,21 +4,32 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def new
     @book = Book.new
   end
-  
+
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to '/'
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      render :new
+    end
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
-  
+ 
+ def destroy
+   book = Book.find(params[:id])
+   book.destroy
+   redirect_to books_path
+ end
+ 
   private
   def book_params
     params.permit(:title, :body)

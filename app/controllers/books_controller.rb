e4e0fcd4215b914f81864d8175eb7b36c03b6012
103nanami <alice_10373@yahoo.co.jp>
2021-10-20 10:16:45 +0000

    Finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,6 +16,7 @@ class BooksController < ApplicationController
     @books = Book.all
     @book = Book.new(book_params)
     if @book.save
+      flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
       render :index
@@ -25,17 +26,26 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
- 
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+
  def destroy
    book = Book.find(params[:id])
    book.destroy
    flash[:notice] = "Book was successfully destroyed."
    redirect_to books_path
  end
- 
-  private
-  def book_params
-    params.require(:book).permit(:title, :body)
-  end
 
-end
+  private
+    def book_params
+      params.require(:book).permit(:title, :body)
+    end
+  end
